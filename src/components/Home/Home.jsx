0e2f2cf1fe317deab77a06b657fb34d6c1f2ff99
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-10-28 22:41:47 +0600

    movie list display

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,20 +2,23 @@ import { useEffect } from "react";
 import MovieListing from "../movieListing/MovieListing";
 import MovieApi from "../../common/apis/MovieApi";
 import { APIKey } from "../../common/apis/MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../feature/movies/movieSlice";
 
 const Home = () => {
+  const MovieText = "Harry";
+  const dispatch = useDispatch();
   useEffect(() => {
-    const MovieText = "Harry";
     const fetchMovies = async () => {
       const response = await MovieApi.get(
         `?apikey=${APIKey}&s=${MovieText}&type=movie`
       ).catch((err) => {
         console.log("ERR:", err);
       });
-      console.log("The Api Response:", response);
+      dispatch(addMovies(response.data));
     };
     fetchMovies();
-  }, []);
+  }, [dispatch]);
   return (
     <div>
       <div className="banner-img"></div>
--- src/components/movieListing/MovieListing.jsx
+++ src/components/movieListing/MovieListing.jsx
@@ -1,9 +1,27 @@
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../../feature/movies/movieSlice";
+import MovieCard from "../movieCard/MovieCard";
+import "./MovieListing.scss";
+
 const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+  console.log("movies:", movies);
+
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
-        <div className="movie-container"></div>
+        <div className="movie-container">
+          {movies.Response === "True" ? (
+            movies.Search.map((movie, index) => (
+              <MovieCard key={index} data={movie}></MovieCard>
+            ))
+          ) : (
+            <div className="movies-error">
+              <h3>{movies.Error}</h3>
+            </div>
+          )}
+        </div>
       </div>
       <div className="show-list">
         <h2>Shows</h2>
